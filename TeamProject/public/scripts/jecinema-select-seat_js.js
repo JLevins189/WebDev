c4deee9da62f987d9c2ae00f279918ebdb47080f
JLev189 <jacklevins@hotmail.com>
2021-11-10 14:50:51 +0000

    update seat booking

--- TeamProject/public/scripts/jecinema-select-seat_js.js
+++ TeamProject/public/scripts/jecinema-select-seat_js.js
@@ -1,7 +1,58 @@
-$(document).ready(function() 
-{
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = $('#count');
+const total = $('#total');
 
 
+populateUI();
 
-    
+let ticketPrice = 10;
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', ticketPrice);
+}
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  $('#count').text(selectedSeatsCount);
+  $('#total').text(selectedSeatsCount * ticketPrice);
+  
+}
+
+// Get data from localstorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+}
+
+// Seat click event
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
 });
+
+// Initial count and total set
+updateSelectedCount();
