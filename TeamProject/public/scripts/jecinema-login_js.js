bf9c0bc405ad83707be804a54aeefd0031c6f989
ethanm32 <c19305451@mytudublin.ie>
2021-11-29 13:09:29 +0000

    comments

--- TeamProject/public/scripts/jecinema-login_js.js
+++ TeamProject/public/scripts/jecinema-login_js.js
@@ -84,28 +84,28 @@ $(document).ready(function() {
         }
     });  
 
+    // searches for matching strings when the search button is clicked  
     $('#search').click(function() { 
-		let searchInput = $('#searchIn').val();
-		let movieArray = [];
-		movieArray.push('savingprivateryan');
-		movieArray.push('thegodfather');
-		movieArray.push('pawpatrol');
-		movieArray.push('thelionking');
-		movieArray.push('notimetodie');
-		movieArray.push('deadlycuts');
-		console.log('hello');
-		console.log(searchInput);
-		let searchInputCleaned = searchInput.replace(/[^A-Z0-9]/ig, '').toLowerCase();
-		console.log(searchInputCleaned);
+    let searchInput = $('#searchIn').val();
+    let movieArray = [];
+    //pushes movies to the array
+    movieArray.push('savingprivateryan');
+    movieArray.push('thegodfather');
+    movieArray.push('pawpatrol');
+    movieArray.push('thelionking');
+    movieArray.push('notimetodie');
+    movieArray.push('deadlycuts');
+    //cleans the search entry and ensures it is lowercase and without special characters
+    let searchInputCleaned = searchInput.replace(/[^A-Z0-9]/ig, '').toLowerCase();
 
-		if(jQuery.inArray(searchInputCleaned, movieArray) !== -1) {
-			console.log("is in array");
-			window.open("http://localhost:3000/" + searchInputCleaned, '_self');
-		} else {
-            window.open("http://localhost:3000/no-results",'_self');
-		}
+    //opens up a new page with the film information
+    if(jQuery.inArray(searchInputCleaned, movieArray) !== -1) {
+        window.open("http://localhost:3000/" + searchInputCleaned, '_self');
+    } else {
+        window.open("http://localhost:3000/no-results",'_self');
+    }
 
-	});
+    });
     
 });
 
